51fe27694d1b218a426ccb3b45c02848b78e896d
Yeasir H <yeasir01@gmail.com>
2023-09-24 02:00:31 -0700

    socket middleware done

--- api/middleware/decodeJwtMiddleware.js
+++ api/middleware/decodeJwtMiddleware.js
@@ -3,26 +3,32 @@ import env from "../config/env.js";
 //Socket.io middleware
 const decodeToken = async (socket, next) => {
     try {
-        const token = socket.handshake.auth.token;
+        const bearerToken = socket.handshake.auth.token;
 
-        if (!token) {
+        if (!bearerToken) {
             throw Error("Token is missing.");
         }
 
-        const options = { headers: { Authorization: token } };
-        const response = await fetch(
-            env.AUTH0_DOMAIN + "/userinfo",
-            options
-        );
+        const options = {
+            method: "GET",
+            headers: {
+                Authorization: bearerToken,
+                "Content-Type": "application/json",
+            },
+        };
+
+        const response = await fetch(`${env.AUTH0_DOMAIN}/userinfo`, options);
         const data = await response.json();
 
         socket.user = {
+            id: data.sub,
             email: data.email,
+            picture: data.picture,
         };
 
         next();
     } catch (err) {
-        console.error(err);
+        console.error(err.message);
         next(Error("Unauthorized"));
     }
 };
--- api/socket/socketHandler.js
+++ api/socket/socketHandler.js
@@ -1,29 +1,19 @@
 import { Socket } from "socket.io";
 
-let users = [];
+const onlineUsers = new Map();
 
 /**
  * Custom socket handler function.
  * @param {Socket} socket - The Socket.IO socket instance.
  */
 const handleSocketRequest = (socket) => {
-    //Create a new user on connection
-    const user = {
-        email: socket.user.email,
-        socket: socket.id,
-    };
-
-    //Add user to the users array
-    users.push(user);
     
-    //print all connected users
-    console.log("Current Users: ", users);
+    onlineUsers.set(socket.id, socket.user.email)
+    console.log("Online Users: ", onlineUsers);
 
-    //Setup listeners
-    socket.on("disconnect", (err) => {
-        console.log("Disconnect Error:", err);
-        users = users.filter(user=> user.socket != socket.id)
-        console.log("Current Users: ", users)
+    socket.on("disconnect", () => {
+        onlineUsers.delete(socket.id)
+        console.log("Online Users: ", onlineUsers)
     });
 };
 
