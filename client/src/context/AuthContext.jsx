6c751bb0d3a79168237314d640f7a63cd1d2aaeb
Yeasir H <yeasir01@gmail.com>
2023-12-29 22:38:41 -0800

    fixed bug causing endless refresh

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -14,7 +14,7 @@ function AuthProvider({ children }) {
         domain: process.env.REACT_APP_AUTH0_DOMAIN,
         clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
         authorizationParams: {
-            redirect_uri: window.location.origin + "/dashboard",
+            redirect_uri: window.location.origin,
             audience: process.env.REACT_APP_AUTH0_AUDIENCE,
         },
         onRedirectCallback
